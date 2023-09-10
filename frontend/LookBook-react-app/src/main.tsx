ae4baef4bc1211d37bdb056071761ef8f0d68fcc
kennytruong001 <67294415+kennytruong001@users.noreply.github.com>
2023-09-10 13:03:27 -0700

    removed unnecessary react folder

--- frontend/LookBook-react-app/src/main.tsx
+++
@@ -1,7 +0,0 @@
-import '@builder.io/qwik/qwikloader.js'
-
-import { render } from '@builder.io/qwik'
-import  App  from './app.tsx'
-import 'bootstrap/dist/css/bootstrap.css'
-
-render(document.getElementById('app') as HTMLElement, <App />)
